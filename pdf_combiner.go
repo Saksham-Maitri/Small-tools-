2259d8b91a87e45ac66e23299878babd4879b25f
Saksham Maitri <142467360+Saksham-Maitri@users.noreply.github.com>
2024-11-14 00:03:14 +0530

    Update pdf_combiner.go

--- pdf_combiner.go
+++ pdf_combiner.go
@@ -11,23 +11,19 @@ import (
 )
 
 func main() {
-	// Define and parse the flag for PDF files
 	pdfs := flag.String("pdfs", "", "Comma-separated list of PDF files to merge")
 	output := flag.String("output", "", "Name of merged file")
 	flag.Parse()
 
-	// Split the input PDFs and check if the flag was provided
 	inputFiles := strings.Split(*pdfs, ",")
 	if len(inputFiles) == 0 || *pdfs == "" {
 		fmt.Println("Please provide PDF files using -pdfs flag, e.g., combiner -pdfs=\"file1.pdf,file2.pdf\"")
 		os.Exit(1)
 	}
 
-	// Determine the directory of the first input file for storing the merged output
 	outputDir := filepath.Dir(inputFiles[0])
 	outputFile := filepath.Join(outputDir, *output+".pdf")
 
-	// Merge the PDF files, passing `nil` for the configuration
 	err := api.MergeCreateFile(inputFiles, outputFile, false, nil)
 	if err != nil {
 		fmt.Printf("Error merging PDFs: %v\n", err)
